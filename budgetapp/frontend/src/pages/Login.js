19719bf1bbfeb453f5c8fecd5c8cfeca3964e02e
Aazain <uhzaynraf@gmail.com>
2025-01-18 15:35:54 -0500

    initial commit

--- budgetapp/frontend/src/pages/Login.js
+++ budgetapp/frontend/src/pages/Login.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+    }
+
     return(
-        <h1>Login</h1>
+        <div>
+            <h1>Login</h1>  
+            <form onSubmit={handleSubmit}></form>
+        </div>
     )
 }
 
